792c9e0aed95bcb20be23664230ba0ca9425bbaf
Joel Hu <a20196510@pucp.edu.pe>
2023-02-03 00:21:58 -0500

    commit6

--- SOME FEATURES.R
+++ SOME FEATURES.R
@@ -4,11 +4,47 @@ library(googledrive)
 drive_auth()
 drive_find(n_max = 10)
 
-drive_find(pattern = "/2022 - 2/*.zip", n_max = 100)
+#drive_find(pattern = "/2022 - 2/*.zip", n_max = 100)
 
-drive_ls(path = '/2022 - 2/', pattern = '*.*')
+#drive_ls(path = '/2022 - 2/', pattern = '*.*')
 
 
   
-  comilla simple <- alt + 39 <- '' E♠
+  comilla simple <- alt + 39 <- ''
   comillas invertidas <- alt + 96 <- ``
+  
+  alt <- para seleccionar los paneles del menu superior
+  
+  
+  MAS ATAJOS:
+  
+  restart R -> ctrl + shift + f10
+  
+  # annotater:
+  
+  library(janitor) # Simple Tools for Examining and Cleaning Dirty Data, CRAN v2.1.0
+  library(tidyverse) # Easily Install and Load the 'Tidyverse', CRAN v1.3.2
+  
+  
+  instalar -> datapasta
+  
+  atajo para escribir en varias lineas: ctrl + alt + (flechas hacia abajo o arriba, segun tu uso)
+  
+  
+  
+  # Panel de atajos:
+  tools |> Keyboard Shortcuts Help <- alt + shift + k
+  
+  # insertar 'pipe':
+  ctrl + shift + m <- `|>` 
+  
+  # Operador de asignacion: 
+  alt + - `<-` 
+  
+  # Arrastrar linea: 
+  alt + up o alt + down
+  
+  # ejecutar linea: ctrl + enter
+  
+  
+  
